94b9ccd000117d53ac86bba01a8290a52c1ba121
DURGESH KUMAR <68071906+aditya4-4@users.noreply.github.com>
2023-08-24 14:57:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function fibonacci(num) {
-// your code here
+function fibonacci(n) {
+	let first =0;
+	let second =1;
+	let third = 0;
+	if(n ==0){
+	  return 0;
+	}
+	else if(n == 1 || n ==2){
+	  return 1;
+	}
+	
+	for(let i=3;i<=n;i++){
+	  third = first + second;
+	  first = second;
+	  second = third;
+	}
+	return third;
+	
 }
 
 module.exports = fibonacci;
